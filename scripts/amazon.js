6c4305a5831a188a830720b88409b46f7bddc9b0
Nadia-2025 <nadyah-88@hotmail.com>
2025-04-30 16:40:15 +0200

    feat:Add js functionality to add products into html using the DOM

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,29 +1,3 @@
-const products= [{
-  image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-  name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-  rating: {
-    stars: 4.5,
-    count: 87
-  },
-  priceCents: 1090,
-},
-{image: "images/products/intermediate-composite-basketball.jpg",
-name: "Intermediate Size Basketball",
-rating: {
-  stars: 4,
-  count: 127
-},
-priceCents: 2095},
-{image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-name: "Adults Plain Cotton T-Shirt - 2 Pack",
-rating: {
-  stars: 4.5,
-  count: 56
-},
-priceCents: 799}
-]
-
-
 let productsHTML= "";
 
 products.forEach ((product) =>{
@@ -46,7 +20,7 @@ products.forEach ((product) =>{
     </div>
 
     <div class="product-price">
-      ${product.priceCents / 100}
+      ${(product.priceCents / 100).toFixed(2)}
     </div>
 
     <div class="product-quantity-container">
@@ -74,17 +48,43 @@ products.forEach ((product) =>{
     </div>
 
     <button class="js-add-to-cart-button
-      add-to-cart-button button-primary" data-testid="add-to-cart-button">
+      add-to-cart-button button-primary"  data-product-id="${product.id}">
       Add to Cart
     </button>
   </div>`
 
 });
 
-console.log(productsHTML);
-
-const productContainer= document.querySelector(".js-products-grid");
+const productContainer = document.querySelector(".js-products-grid");
 productContainer.innerHTML = productsHTML;
+
+document.querySelectorAll(".js-add-to-cart-button")
+.forEach ((button) =>{
+  button.addEventListener ("click", ()=> {
+    const productId = button.dataset.productId;
+
+    
+let matchingItem;
+
+    cart.forEach((item) => {
+      if (productId === item.productId){
+        matchingItem = item;
+      }
+    });
+    if (matchingItem) {
+      matchingItem.quantity +=1;
+    } else {
+      cart.push({
+        productId : productId,
+        quantity: 1
+      });
+    }
+
+
+    console.log(cart);
+  });
+});
+
 
 
 
